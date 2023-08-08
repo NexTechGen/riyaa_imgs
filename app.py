c04af633c919f6a7cbdb82c08ab4d74e10fefbf6
NexTechGen <141403098+NexTechGen@users.noreply.github.com>
2023-08-08 09:09:19 +0000

    from img import get_list

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
+from img import get_list
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_jovin():
-  return render_template(
-    "home.html")  # home.html or home_bootstrap.html
+  return render_template("home.html")  # home.html or home_bootstrap.html
 
+
+print(get_list())
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- img.py
+++ img.py
@@ -1,19 +1,19 @@
 import os
 
-list_imges = []
+def get_list():
+  list_imges = []
 
-curent_dir = os.getcwd()
+  curent_dir = os.getcwd()
+  img_file_dir = curent_dir + "/all_imgs"
+  img_file_dir_list = os.listdir(img_file_dir)
 
-img_file_dir = curent_dir + "/all_imgs"
+  for i in range(len(img_file_dir_list)):
+    get_folder = img_file_dir + "/" + img_file_dir_list[i]
+    listOf_get_folder = os.listdir(get_folder)
+    for s in range(len(listOf_get_folder)):
+      get_file = listOf_get_folder[s]
+      imgs = get_folder + "/" + get_file
+      list_imges.append(imgs)
 
-img_file_dir_list = os.listdir(img_file_dir)
+  return list_imges
 
-for i in range(len(img_file_dir_list)):
-  get_folder = img_file_dir + "/" + img_file_dir_list[i]
-  listOf_get_folder = os.listdir(get_folder)
-  for s in range(len(listOf_get_folder)):
-    get_file = listOf_get_folder[s]
-    imgs = get_folder + "/" + get_file
-    list_imges.append(imgs)
-
-print(list_imges)
