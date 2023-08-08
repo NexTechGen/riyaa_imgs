2f11f3a7da63788fb9fd773643a4c3ad1566d92a
NexTechGen <141403098+NexTechGen@users.noreply.github.com>
2023-08-08 14:22:27 +0000

    put the img in web

--- app.py
+++ app.py
@@ -6,10 +6,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_jovin():
-  return render_template("home.html")  # home.html or home_bootstrap.html
+  return render_template("home.html", paths = get_list() )  # home.html or home_bootstrap.html
 
-
-print(get_list())
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- img.py
+++ img.py
@@ -1,19 +1,23 @@
 import os
 
+
 def get_list():
   list_imges = []
 
   curent_dir = os.getcwd()
-  img_file_dir = curent_dir + "/all_imgs"
-  img_file_dir_list = os.listdir(img_file_dir)
+  img_file_dir = curent_dir + "/static/all_imgs"
 
-  for i in range(len(img_file_dir_list)):
-    get_folder = img_file_dir + "/" + img_file_dir_list[i]
-    listOf_get_folder = os.listdir(get_folder)
-    for s in range(len(listOf_get_folder)):
-      get_file = listOf_get_folder[s]
-      imgs = get_folder + "/" + get_file
-      list_imges.append(imgs)
+  folders = os.listdir(img_file_dir)
+
+  for folder in range(len(folders)):
+    files = os.listdir(img_file_dir + "/" + folders[folder])
+    for file in files:
+      path = "static/all_imgs/" + folders[folder] + "/" + file
+      list_imges.append(path)
 
   return list_imges
 
+
+#for x in get_list(): print(x)
+
+get_list()
