e088973e2fc5a7699192687cf3f486593a3d4d6e
NexTechGen <141403098+NexTechGen@users.noreply.github.com>
2023-08-09 03:34:55 +0000

    change banner

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ from img import get_list
 
 app = Flask(__name__)
 
+banner_name ="Riyaa Galaxy"
+icon_img = "static/img_20190208_wa0005_gpW_icon.ico"
 
 @app.route("/")
 def hello_jovin():
-  return render_template("home.html", paths = get_list() )  # home.html or home_bootstrap.html
+  return render_template("home.html", 
+                         paths = get_list(), 
+                         name=banner_name, 
+                          banner_imges = icon_img)  # home.html or home_bootstrap.html
 
 
 if __name__ == "__main__":
